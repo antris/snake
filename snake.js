e439ee1c976cb8745a33ed4bd46aece845e0fc70
Anton Rissanen <anton.rissanen@reaktor.fi>
2013-09-06 13:16:36 +0300

    Draw tail

--- snake.js
+++ snake.js
@@ -34,10 +34,19 @@
     "width": coordToPx(1),
     "height": coordToPx(1)
   })
+  var createTailPiece = function() {
+    return $('<div>').addClass('tail').css({
+      "position": "absolute",
+      "background": "green",
+      "width": coordToPx(1),
+      "height": coordToPx(1)
+    })
+  }
   var randomPosition = function() {
     return [Math.floor(Math.random() * GRID_WIDTH), Math.floor(Math.random() * GRID_HEIGHT)]
   }
-  var ticks = Bacon.interval(300).takeUntil(gameEnd)
+  var equals = function(x) { return function(y) { return x === y }}
+  var ticks = Bacon.interval(150).takeUntil(gameEnd)
   var tickCounter = ticks.map(1).scan(0, add)
   var $counter = $('#counter')
   var keyPresses = $(document).asEventStream('keydown').map('.keyCode')
@@ -46,8 +55,12 @@
   var direction = directionChanges.scan([0, 0], second)
   var headPosition = direction.sampledBy(ticks).scan([5, 5], incrementDirection)
   var applePosition = new Bacon.Bus()
-  var appleEaten = headPosition.combine(applePosition, positionEquals).filter(function(x) { return x })
+  var appleEaten = headPosition.combine(applePosition, positionEquals).filter(equals(true))
+  var tailSize = appleEaten.scan(1, function(x) { return x + 1 })
   appleEaten.onValue(function() { applePosition.push(randomPosition()) })
+  var tail = headPosition.scan([], function(memo, pos) { return memo.concat([pos]) }).combine(tailSize, function(headPosition, tailSize) {
+    return _(headPosition).last(tailSize)
+  })
   gameEnd.onValue(function() {
     $('#grid').empty()
   })
@@ -61,4 +74,18 @@
   headPosition.onValue(function(pos) { $head.css({ "left": coordToPx(pos[0]), "top": coordToPx(pos[1]) }) })
   applePosition.onValue(function(pos) { $apple.css({ "left": coordToPx(pos[0]), "top": coordToPx(pos[1]) }) })
   applePosition.push(randomPosition())
+  var tailPieces = tailSize.map(function(size) {
+    return _(size).chain().range().map(createTailPiece).value()
+  })
+  Bacon.combineTemplate({ tail: tail, pieces: tailPieces }).onValue(function(data) {
+    $('#grid .tail').remove()
+    _(data.tail).each(function(pos, index) {
+      var piece = data.pieces[index]
+      $('#grid').append(piece)
+      piece.css({
+        left: coordToPx(pos[0]),
+        top: coordToPx(pos[1])
+      })
+    })
+  })
 })(jQuery, Bacon)
